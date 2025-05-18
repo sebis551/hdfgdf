9a363b2466b57f5b6e9296703f913862daccbe9a
sebastian.badea0506 <sebastina.badea0506@stud.acs.upb.ro>
2025-05-18 07:56:36 +0300

    gfdg

--- scriptfiles/start-local.sh
+++ scriptfiles/start-local.sh
@@ -4,5 +4,5 @@ docker build -t fitness-app ./business-logic-service-main
 
 
 echo "Deploying stack to Docker Swarm..."
-docker stack deploy -c /docker-files-main fitness-app
+docker stack deploy -c ./docker-files-main fitness-app
 
