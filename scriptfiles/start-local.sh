2f9b780812850588158fc53a84ddacf37ed112b1
sebastian.badea0506 <sebastina.badea0506@stud.acs.upb.ro>
2025-05-18 07:52:07 +0300

    gfdg

--- scriptfiles/start-local.sh
+++ scriptfiles/start-local.sh
@@ -1,6 +1,6 @@
 echo "Building Docker images..."
-docker build -t /docker-files-main/auth-service ./auth-service-main
-docker build -t /docker-files-main/fitness-app ./business-logic-service-main
+docker build -t auth-service ./auth-service-main
+docker build -t fitness-app ./business-logic-service-main
 
 
 echo "Deploying stack to Docker Swarm..."
